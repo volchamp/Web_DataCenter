eedc1661c961f111633301783c9e3273a1cc15d6
yangxch <xch_yang@126.com>
2023-03-31 18:06:15 +0800

    update im.

--- static/js/webApi.js
+++ static/js/webApi.js
@@ -11,7 +11,7 @@ if(urlpath.indexOf("127.0.0.1")>-1)
 {
 	// ip="124.222.4.40";
 	// ip="127.0.0.1";
-	imgurl ='http://'+ip+':80/files/';//文件访问路径
+	imgurl ='http://'+ip+':8091/';//文件访问路径
 	// 地址
 	curl='http://'+ip+':8088/api-im';
 	wsIm='ws://'+ip+':8088/api-im/websocket/';
